ac87d4dfa4c8a5e67529d4da918c3a2d3223ca5c
Isidoro Torrico M <itorrico.m@gmail.com>
2025-01-20 16:56:12 -0400

    Loops y Bucles

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-// Declarion de variables
+
 let numeroSecreto = 7;
-let numeroUsuario = prompt("Ingrese un numero entre 1 y 10: ");
-console.log(numeroUsuario);
-/*
-Este codigo realiza
-la comparacion
-*/
-if(numeroSecreto == numeroUsuario) {
-    //Acertamos, fue verdadea la condicion
-    alert(`Acertaste, el numero es: ${numeroUsuario}`);
-} else {
-    if(numeroUsuario > numeroSecreto) {
-        alert('El numero secreto es menor');
-    }else{
-        alert('El numero secreto es mayor')
+let numeroUsuario = 0;
+
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = prompt("Ingrese un numero entre 1 y 10: ");
+    console.log(numeroUsuario);
+
+    if (numeroSecreto == numeroUsuario) {
+        alert(`Acertaste, el numero es: ${numeroUsuario}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El numero secreto es menor');
+        } else {
+            alert('El numero secreto es mayor')
+        }
+        alert("lo sieto, no acertaste el numero");
     }
-    //La condicion no se cumplio
-    alert("lo sieto, no acertaste el numero");
 }
+
