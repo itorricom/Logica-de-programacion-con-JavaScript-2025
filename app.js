fe0ec9acc135acdeca465b4bf95c5d2ac55e0e03
Isidoro Torrico M <itorrico.m@gmail.com>
2025-01-20 17:56:33 -0400

    Contador de Intentos

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 
 let numeroSecreto = 7;
 let numeroUsuario = 0;
+let intento = 1;
+let palabraVeces = 'vez';
 
 while (numeroUsuario != numeroSecreto) {
     numeroUsuario = prompt("Ingrese un numero entre 1 y 10: ");
     console.log(numeroUsuario);
 
     if (numeroSecreto == numeroUsuario) {
-        alert(`Acertaste, el numero es: ${numeroUsuario}`);
+        alert(`Acertaste, el numero es: ${numeroUsuario}, Lo hiciste en ${intento} ${palabraVeces}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El numero secreto es menor');
@@ -15,6 +17,8 @@ while (numeroUsuario != numeroSecreto) {
             alert('El numero secreto es mayor')
         }
         alert("lo sieto, no acertaste el numero");
+        intento = intento + 1;
+        palabraVeces ='veces';
     }
 }
 
