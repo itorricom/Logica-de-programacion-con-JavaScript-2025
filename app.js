b305a22ce312327a351ff57e33330b2f235425cd
Isidoro Torrico M <itorrico.m@gmail.com>
2025-01-21 12:07:36 -0400

    Desafío Loops

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/* 
 let numeroSecreto = 7;
 let numeroUsuario = 0;
 let intento = 1;
@@ -20,5 +20,44 @@ while (numeroUsuario != numeroSecreto) {
         intento = intento + 1;
         palabraVeces ='veces';
     }
+} */
+
+/* 
+    Desafío Loops
+Hemos llegado a otra lista de actividades (no obligatorias) para que practiques y refuerces aún más tu aprendizaje. ¿Vamos a hacerlo? Desafíos:
+
+Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+Crea un programa de cuenta regresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+ */
+console.log('\n============== 1 ==================');
+
+let contatador = 1;
+while (contatador < 11) {
+    console.log(contatador);
+    contatador++;    
+}
+console.log('\n============== 2 ==================');
+
+let count = 10;
+while (count > -1) {
+    console.log(count);    
+    count--;
 }
 
+console.log('\n============== 3 ==================');
+let numero = prompt('Un numero para cuenta regresiva');
+while (numero > -1) {
+    console.log(numero);    
+    numero--;
+}
+
+console.log('\n============== 4 ==================');
+
+let numeroTope = prompt('Un numero para cuenta progresiva');
+let num = 0
+while (num <=  numeroTope) {
+    console.log(num);
+    num++;    
+}
