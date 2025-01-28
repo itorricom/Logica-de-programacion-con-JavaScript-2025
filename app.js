e74e4884aa4c26f0701360816f2681ec9572d769
Isidoro Torrico M <itorrico.m@gmail.com>
2025-01-27 23:44:25 -0400

    Operador Ternario

--- app.js
+++ app.js
@@ -2,24 +2,27 @@
 let numeroSecreto = 7;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
+//let palabraVeces = 'vez';
+let maximosIntentos = 3;
 
 while (numeroUsuario != numeroSecreto) {
     numeroUsuario = prompt("Ingrese un numero entre 1 y 10: ");
     console.log(numeroUsuario);
 
     if (numeroSecreto == numeroUsuario) {
-        alert(`Acertaste, el numero es: ${numeroUsuario}, Lo hiciste en ${intentos} ${palabraVeces}`);
+        alert(`Acertaste, el numero es: ${numeroUsuario}, Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El numero secreto es menor');
         } else {
             alert('El numero secreto es mayor')
         }
-        intentos = intentos + 1;
-        palabraVeces ='veces';
-        if(intentos > 3 ){
-            alert("Has superado el numero de intentos permitidos. El numero secreto era: " + numeroSecreto);
+        //intentos = intentos + 1;
+        //intentos += 1
+        intentos++;
+        //palabraVeces ='veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al numero maximo de ${maximosIntentos} intentos`);
             break;
         }
     }
